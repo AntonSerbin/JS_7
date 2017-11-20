fa9decaff36b5ad8fd3c17c3c0afac3f700c9934
anton <antons.zn@gmail.com>
2017-11-20 14:47:36 +0200

    version with mentioned mistakes

--- script7.js
+++ script7.js
@@ -5,73 +5,80 @@ let counterDivTask1 = 0;
 
 	// function to delete old "empty fields
 function clearP(){
-		var allP=parentDiv1.querySelectorAll('p');
-		for (var i= 0; i<allP.length;i++)
-			{allP[i].style.display = "none";};
-				};
+	var allP=parentDiv1.querySelectorAll('p');
+	for (var i= 0; i<allP.length;i++) {
+		allP[i].style.display = "none";
+	};
+};
 
 	// function adding new possition
 function addInputTask1(number){
 			// add new div
-		var divSmall = document.createElement('div');
-		divSmall.classList.add("task1");
-		divSmall.id = "divNumber"+number;
-		parentDiv1.appendChild(divSmall);
+	var divSmall = document.createElement('div');
+	divSmall.classList.add("task1");
+	divSmall.id = "divNumber"+number;
+	parentDiv1.appendChild(divSmall);
 		
-		// add new inptup inside new div
-		var inp = document.createElement('input');
-		inp.classList.add("task1");
-		inp.value = number;
-		divSmall.appendChild(inp);
+	// add new inptup inside new div
+	var inp = document.createElement('input');
+	inp.classList.add("task1");
+	inp.value = number;
+	divSmall.appendChild(inp);
 
-		//add new button inside new div
-		var but = document.createElement('button');
-		but.innerHTML="-";
-		but.id="number"+number;
-		divSmall.appendChild(but);
+	//add new button inside new div
+	var but = document.createElement('button');
+	but.innerHTML="-";
+	but.id="number"+number;
+	divSmall.appendChild(but);
 		
-					//function to delete smallDiv(with input and button)
-					but.addEventListener('click', function(){
-					var nameDiv = "divNumber"+number;
-					var divTask1 = document.getElementById(nameDiv);
-					parentDiv1.removeChild(divTask1);
-									});
-								};
-	// function to the button plus and setting of limit of maximum inputs
+	//function to delete smallDiv(with input and button)
+	but.addEventListener('click', function(){
+		var nameDiv = "divNumber"+number;
+		var divTask1 = document.getElementById(nameDiv);
+		parentDiv1.removeChild(divTask1);
+	});
+};
+
+	// function for the button plus and setting of limit of maximum inputs
 var plusTask1 = document.querySelector("button.plus");
 plusTask1.addEventListener("click", function(){
- 					var inputTask1 = document.querySelectorAll("div.task1");
- 						if (inputTask1.length<=7) {
-								counterDivTask1++;
-								addInputTask1(counterDivTask1);
-								;}
-						else {document.getElementById("resultFieldTask1").innerHTML = `Limit`;};
- 							});
+ 	var inputTask1 = document.querySelectorAll("div.task1");
+ 	if (inputTask1.length<=7) {
+		counterDivTask1++;
+		addInputTask1(counterDivTask1);
+	}
+	else {document.getElementById("resultFieldTask1").innerHTML = `Limit`;
+	};
+});
 
 	//function to calculate final result
 var resultTask1 = document.getElementById("resultTask1");
 resultTask1.onclick = function(){
-
-clearP(); //clean old alert "empty field"
-
+	clearP(); //clean old alert "empty field"
 	document.getElementById("resultFieldTask1").innerHTML = "";
-		var divSmall = document.createElement('div');
-		var inputTask1 = document.querySelectorAll("input.task1");
-			//check the empty inputs and add new p "empty element"		
-			for (var i = 0; i < inputTask1.length; i++) {
-					if (inputTask1[i].value==""){
-						inputTask1[i].style.borderColor = "red";
-						var smallDiv = inputTask1[i].parentNode;
-						var textP = document.createElement('p');
-						textP.id = "empty";
-						textP.innerHTML = "Empty input";
-						textP.style.display = "inline";
-						smallDiv.appendChild(textP);
-									}
-						else {inputTask1[i].style.borderColor = "initial"};
-					if ((radioTask2[0].checked==true) && (i%2==0)) {document.getElementById("resultFieldTask1").innerHTML += `${inputTask1[i].value} `;};
-					if ((radioTask2[1].checked==true) && (i%2!=0)) {document.getElementById("resultFieldTask1").innerHTML += `${inputTask1[i].value} `;};
-					if (radioTask2[2].checked==true) {document.getElementById("resultFieldTask1").innerHTML += `${inputTask1[i].value} `;};
+	var divSmall = document.createElement('div');
+	var inputTask1 = document.querySelectorAll("input.task1");
+	
+	//check the empty inputs and add new p "empty element"		
+	for (var i = 0; i < inputTask1.length; i++) {
+		if (inputTask1[i].value==""){
+			inputTask1[i].style.borderColor = "red";
+			var smallDiv = inputTask1[i].parentNode;
+			var textP = document.createElement('p');
+			textP.id = "empty";
+			textP.innerHTML = "Empty input";
+			textP.style.display = "inline";
+			smallDiv.appendChild(textP);
+		}
+		else {inputTask1[i].style.borderColor = "initial"};
+		if ((radioTask2[0].checked==true) && (i%2==0)) {
+			document.getElementById("resultFieldTask1").innerHTML += `${inputTask1[i].value} `;
+		};
+		if ((radioTask2[1].checked==true) && (i%2!=0)) {
+			document.getElementById("resultFieldTask1").innerHTML += `${inputTask1[i].value} `;
+		};
+		if (radioTask2[2].checked==true) {
+			document.getElementById("resultFieldTask1").innerHTML += `${inputTask1[i].value} `;};
 								};
 							};
 // end tasks 1-3
@@ -87,19 +94,22 @@ buttontask4.onclick = function() {
     var windowButton1 = optionWindow.document.querySelector('#windowButton1');
     var windowButton2 = optionWindow.document.querySelector('#windowButton2');
 			
-			windowButton1.onclick = function()	{
-					resultFieldTask4.innerHTML = "You have choosen option 1";
-					optionWindow.close();		};
+	windowButton1.onclick = function()	{
+		resultFieldTask4.innerHTML = "You have choosen option 1";
+		optionWindow.close();		
+	};
 
-			windowButton2.onclick = function()	{
-					resultFieldTask4.innerHTML = "You have choosen option 2";
-					optionWindow.close();		};
-											};
+	windowButton2.onclick = function()	{
+		resultFieldTask4.innerHTML = "You have choosen option 2";
+		optionWindow.close();		
+	};
+};
+
 // task 4 end; task 5 start								
 var liTask5 = document.querySelectorAll(".liTask5");
 for (var i=0; i<liTask5.length; i++) {
 	liTask5[i].onclick = function(){this.classList.toggle("colorTask5");};
-	};
+};
 var buttonTask5 = document.querySelector("#buttonTask5");
 var luTask5 = document.querySelector("#ulTask5");
 buttonTask5.addEventListener("click", function(){
@@ -109,58 +119,104 @@ buttonTask5.addEventListener("click", function(){
 	newLiTask5.innerHTML = "new element "+ (luTask5.children.length+1);
 	luTask5.appendChild(newLiTask5);
 });
+
 // task 5 end; task 6 start								
+var divTask6 = document.querySelector("#divTask6");
+var divText6 = document.querySelector("#divText6");
+var counterIn = 0;
+var counterOut = 0;
+var actionPermission = true;
+
+divTask6.addEventListener("mouseenter", displayBlock);
+divTask6.addEventListener("mouseleave", hideBlock);
+
 	
-	var divTask6 = document.querySelector("#divTask6");
-	var divText6 = document.querySelector("#divText6");
-	function displayBlock(){divText6.classList.add('displayBlock');};
-	function hideBlock(){setTimeout(function(){divText6.classList.remove('displayBlock')},2500);};
-	divTask6.addEventListener("mouseleave", function(){hideBlock();});
-	divTask6.addEventListener("mouseenter", function(){displayBlock()});
+
+function displayBlock(){
+	counterIn++;
+		if ((counterIn>counterOut) && (actionPermission==true)) {
+			mogno=false;
+			divText6.classList.add('displayBlock');
+			setTimeout	(function(){	
+				actionPermission=true;
+				if ((counterIn==counterOut)) {
+					counterOut--;
+					hideBlock();
+				};
+			},1500
+		);
+	};
+};
+
+function hideBlock(){
+	counterOut++;
+		if ((counterIn==counterOut) && (actionPermission==true)){
+			actionPermission=false;
+			setTimeout	(function()	{
+				if (counterIn==counterOut) {
+					divText6.classList.remove('displayBlock')
+				};
+				actionPermission=true;
+			},2500
+		);
+	};
+};
+
+
+
 // task 6 end; task 7,8 start	
-		var resultInfo7 = document.querySelector("#resultInfo7");
+var resultInfo7 = document.querySelector("#resultInfo7");
 
-	var images7 = document.querySelectorAll("img");
-		for (var i = 0; i < images7.length; i++) {
-			images7[i].addEventListener("mouseenter", function(){infoImage(this);});
+var images7 = document.querySelectorAll("img");
+	for (var i = 0; i < images7.length; i++) {
+		images7[i].addEventListener("mouseenter", function(){infoImage(this);});
+	};
+function infoImage(image){
+
+	resultInfo7.innerHTML = `<b>image link:</b> ${image.currentSrc} <br> <b>image alternative name:</b> ${image.alt}`;
+		if (image.currentSrc.length==0 || image.alt.length==0 ){
+			resultInfo7.style.color = 'red'; 
+			resultInfo7.innerHTML += "    !!!!some parametres are empty!"
+		}
+		else resultInfo7.style.color = "initial";
+		if (image.alt.length==0 ){
+			image.style.border = "solid 3px red";
+			image.style.opacity = "0.75";
 		};
-	function infoImage(image){
-			 resultInfo7.innerHTML = `<b>image link:</b> ${image.currentSrc} <br> <b>image alternative name:</b> ${image.alt}`;
-			if (image.currentSrc.length==0 || image.alt.length==0 ){
-												resultInfo7.style.color = 'red'; 
-												resultInfo7.innerHTML += "    !!!!some parametres are empty!"}
-				else resultInfo7.style.color = "initial";
-			if (image.alt.length==0 ){			image.style.border = "solid 3px red";
-												image.style.opacity = "0.75";}
-			};
+};
+var links7 = document.querySelectorAll("a");
+for (var i = 0; i < links7.length; i++) {
+	links7[i].addEventListener("mouseenter", function(){
+		infoLink(this);
+	});
+};
 
-	var links7 = document.querySelectorAll("a");
-		for (var i = 0; i < links7.length; i++) {
-			links7[i].addEventListener("mouseenter", function(){infoLink(this);});
-		};
-
-	function infoLink(link){
-			resultInfo7.innerHTML = `<b>link title:</b> ${link.title} <br> <b>link target:</b> ${link.target}`;
-			if (link.title.length==0 || link.target.length==0 ){
-												resultInfo7.style.color = 'red'; 
-												resultInfo7.innerHTML += "   !!!!some parametres are empty!"}
-				else resultInfo7.style.color = "initial";
-			};
+function infoLink(link){
+	resultInfo7.innerHTML = `<b>link title:</b> ${link.title} <br> <b>link target:</b> ${link.target}`;
+	if (link.title.length==0 || link.target.length==0 ){
+		resultInfo7.style.color = 'red'; 
+		resultInfo7.innerHTML += "   !!!!some parametres are empty!"
+	}
+	else resultInfo7.style.color = "initial";
+};
 // task 7,8 end; task 9 start	
 
 var buttontask9 = document.querySelector('#buttonTask9');
 var buttonDeleteHTask9 = document.querySelector("#buttonDeleteHTask9");
 buttontask9.onclick = function(){
-var quantityHTask9 = document.querySelectorAll("h1,h2,h3,h4,h5,h6");
-	if (quantityHTask9.length!=0) {resultFieldTask9.innerHTML = `Quantity of H : ${quantityHTask9.length} pcs`}
-		else {resultFieldTask9.innerHTML = `There is no H of a page`};
+	var quantityHTask9 = document.querySelectorAll("h1,h2,h3,h4,h5,h6");
+	if (quantityHTask9.length!=0) {
+		resultFieldTask9.innerHTML = `Quantity of H : ${quantityHTask9.length} pcs`
+	}
+	else resultFieldTask9.innerHTML = `There is no H of a page`;
 								};
 buttonDeleteHTask9.onclick = function(){
-var quantityHTask9 = document.querySelectorAll("h1,h2,h3,h4,h5,h6");
+	var quantityHTask9 = document.querySelectorAll("h1,h2,h3,h4,h5,h6");
 	for (var i = 0; i < quantityHTask9.length; i++) {
-			var parent = quantityHTask9[i].parentNode;
-			parent.removeChild(quantityHTask9[i]);};
-								};
+		var parent = quantityHTask9[i].parentNode;
+		parent.removeChild(quantityHTask9[i]);
+	};
+};
 
 // task 9 end; task 10 start	
 var titleTask10 = document.head.querySelector('title')||false;
